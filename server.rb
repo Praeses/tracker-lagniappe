b61e11efcb7edb5e89e2bf32e47664b447aae221
ksimpson <simp@hotmail.com>
2011-01-29 01:55:16 -0600

    added haml views for home page and release plan

--- server.rb
+++ server.rb
@@ -6,34 +6,22 @@ configure :production do
   require 'newrelic_rpm'
 end
 
+get '/' do
+  haml :index
+end
+
 class Iteration < ActiveResource::Base
   self.site = "http://www.pivotaltracker.com/services/v3/projects/:project_id"
 end
 
-
-get '/:api_token/:project_id' do
-  Iteration.headers['X-TrackerToken'] = params[:api_token]
-  current_iteration  = Iteration.find(:all, :params => {:project_id => params[:project_id], :group => "current"})
-  iterations_backlog = Iteration.find(:all, :params => {:project_id => params[:project_id], :group => "backlog"})
+get '/releaseplan' do
+  api_token = params[:api_token]
+  project_id = params[:project_id]
+  
+  Iteration.headers['X-TrackerToken'] = api_token
+  current_iteration  = Iteration.find(:all, :params => {:project_id => project_id, :group => "current"})
+  iterations_backlog = Iteration.find(:all, :params => {:project_id => project_id, :group => "backlog"})
   @iterations = current_iteration + iterations_backlog
 
-  haml :milestones
+  haml :releaseplan
 end
-
-__END__
-
-@@ layout
-!!!
-%body
-  = yield
-
-@@ milestones
-
-%h1  Release Plan
-
-- @iterations.each do |iteration|
-  %h2{ :class => 'sprint' } Start #{ iteration.start.strftime("%Y-%m-%d") } - End #{ iteration.finish.strftime("%Y-%m-%d") }
-
-  %ul
-    - iteration.stories.each do |story|
-      %li{ :class => 'story' } #{story.name}
