15ae5d9263bb6115157f6ff7766cb7a80ce521c7
ksimpson <simp@hotmail.com>
2011-02-02 22:57:23 -0600

    added burn rate and items from review

--- server.rb
+++ server.rb
@@ -6,22 +6,27 @@ configure :production do
   require 'newrelic_rpm'
 end
 
+class Iteration < ActiveResource::Base
+  self.site = "https://www.pivotaltracker.com/services/v3/projects/:project_id"
+end
+
 get '/' do
   haml :index
 end
 
-class Iteration < ActiveResource::Base
-  self.site = "http://www.pivotaltracker.com/services/v3/projects/:project_id"
-end
-
-get '/releaseplan' do
+post '/release_plan' do
   api_token = params[:api_token]
   project_id = params[:project_id]
-  
+  @cost = Float(params[:cost])
+
   Iteration.headers['X-TrackerToken'] = api_token
   current_iteration  = Iteration.find(:all, :params => {:project_id => project_id, :group => "current"})
   iterations_backlog = Iteration.find(:all, :params => {:project_id => project_id, :group => "backlog"})
   @iterations = current_iteration + iterations_backlog
+  
+  @total = @iterations.length * @cost
+  
+  #@total = number_to_currency(@iterations.length * @cost)
 
-  haml :releaseplan
+  haml :release_plan
 end
