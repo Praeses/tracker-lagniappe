2bc44427a2e88d0d06aee3d19403aad265b910cc
ksimpson <simp@hotmail.com>
2011-02-13 20:10:02 -0600

    added validation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function validateform(){  
-  if (!isNaN(document.getElementById( "cost" ).value)) 
+function validateform(){
+  if (!isNaN(document.getElementById( "cost" ).value))
     {
       document.getElementById( "costerr" ).className='off';
       return true;
     }
   else
     {
-	  document.getElementById( "costerr" ).className='on'
+      document.getElementById( "costerr" ).className='on'
       return false;
     }
 }
