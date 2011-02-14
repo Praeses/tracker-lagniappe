f517102392ae135aaba090ac0bcaee4a39295a26
ksimpson <simp@hotmail.com>
2011-02-13 20:04:15 -0600

    added validation

--- server.rb
+++ server.rb
@@ -17,16 +17,31 @@ end
 post '/release_plan' do
   api_token = params[:api_token]
   project_id = params[:project_id]
-  @cost = Float(params[:cost])
 
   Iteration.headers['X-TrackerToken'] = api_token
-  current_iteration  = Iteration.find(:all, :params => {:project_id => project_id, :group => "current"})
-  iterations_backlog = Iteration.find(:all, :params => {:project_id => project_id, :group => "backlog"})
-  @iterations = current_iteration + iterations_backlog
-  
-  @total = @iterations.length * @cost
-  
-  #@total = number_to_currency(@iterations.length * @cost)
 
-  haml :release_plan
+  begin
+    current_iteration  = Iteration.find(:all, :params => {:project_id => project_id, :group => "current"})
+    iterations_backlog = Iteration.find(:all, :params => {:project_id => project_id, :group => "backlog"})
+	
+    if !current_iteration.nil? & !iterations_backlog.nil?
+      @iterations = current_iteration + iterations_backlog
+      if !params[:cost].blank?
+        @cost = Float(params[:cost])
+      else
+        @cost = 0
+      end
+
+      @total = @iterations.length * @cost
+  
+      #@total = number_to_currency(@iterations.length * @cost)
+
+      haml :release_plan
+    else
+      haml :no_iterations
+    end	
+	
+  rescue ActiveResource::UnauthorizedAccess
+    haml :unauthorized
+  end
 end
